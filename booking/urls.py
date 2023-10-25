87a0e331a1e99b14dbf6ee85dfeefbd544af17a2
0w3nM <>
2023-10-25 13:37:08 +0000

    Updated the code in the views.py. Added more uls paths in the url.py file. Updated html page names.

--- booking/urls.py
+++ booking/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='home'),
     path('bookings/', views.BookingList.as_view(), name='bookings'),
-
-
+    path('create_reservations/', views.create_reservation, name='create_reservation'),
+    path('update_reservation/', views.update_reservation, name='updat'),
+    path('remove_reservation/', views.remove_reservation, name='remove_reservation'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,25 +1,69 @@
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Reservation
+from .forms import ReservationForm
+
+# View Reservations #
 
 
-def index(request):
-    return render(request, "index.html")
-
-
-class BookingList(generic.ListView):
+class ReservationList(generic.View):
     model = Reservation
-    template_name = 'booking.html'
+    template_name = 'bookings.html'
+    fields = ["name", "day", "time", "party_size"]    
 
+# Create Reservation #
 
-class CreateReservation():
-    model = Reservation
-    template = 'create_reservation.html'
+    def create_reservation(self, request):
+        """
+        User can crate a reseration
+        """
+        if request.method == 'POST':
+            reservation_form = ReservationForm(request.POST)
+            if reservation_form.is_valid():
+                reservation_form.instance.name = request.user
+                reservation_form.save()
+                messages.success(request, 'Your reservation was successul.')
+                return redirect('bookings')
+            else:
+                messages.error(request,
+                'Reservation wasnt successful. Please amend and try again.')
+                reservation_form = ReservationForm()
+                context = {
+                    'form': reservation_form
+                }
+            return render(request, '/templates/create_reservation')
 
-    def update_booking(self, request, booking_id):
+    # Update Reservation #
+
+    def update_reservation(self, request, booking_id):
+        """
+        User can amend a reservation
+        """
         booking = get_object_or_404(Reservation, id=booking_id)
+        if request.method == 'POST':
+            reservation_form = ReservationForm(request.POST)
+            if reservation_form.is_valid():
+                messages.success(request, 
+                 'Your reservation was successully updated.')
+                return redirect('bookings')
+            else:
+                messages.error(request,
+                 'Reservation wsnt updated successfully. Please amend and try again.')
+                reservation_form = ReservationForm()
+                context = {
+                    'form': reservation_form
+                }
+            return render(request, '/templates/update_reservation')
 
-    def remove_booking(self, request, booking_id):
+    # Delete Reservation #
+
+    def remove_reservation(self, request, booking_id):
+        """
+        User can delte a reservation
+        """
         booking = get_object_or_404(Reservation, id=booking_id)
-        booking.delte()
+        booking.delete()
+        messages.success(request,
+        'Your reservation has been sucessfully deleted')       
         return redirect('bookings')
